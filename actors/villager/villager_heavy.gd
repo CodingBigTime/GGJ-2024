b58d18a2f52b8863837c7d27965d4091537c85be
Tomislav Ignjatov <tomislav@codechem.com>
2024-01-28 17:56:50 +0100

    Revert "Remove prints, and make sprint debug only"
    This reverts commit 0e12785f2b5647760a60e6e740acad60854925bf, reversing
    changes made to b18519aab10ef5915466188f810f97a6d144cb3a.

--- actors/villager/villager_heavy.gd
+++ actors/villager/villager_heavy.gd
@@ -60,5 +60,4 @@ func _on_attack_timer_timeout():
 		debug_sphere.radius = self.SLAM_RADIUS
 		debug_sphere.position = self.position
 		get_parent().add_child(debug_sphere)
-	$HeavyAttackParticles.one_shot = true
-	$HeavyAttackParticles.emitting = true
+	$GPUParticles3D.emitting = true
